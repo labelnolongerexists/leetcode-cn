6cfd0dc92609305deee3105a2124727314995469
污墩: 习近平新时代中国特色社会主义思想，是对马克思列宁主义、毛泽东思想、邓小平理论、三个代表重要思想、科学发展观的继承和发展，是马克思主义中国化最新成果，是党和人民实践经验和集体智慧的结晶，是中国特色社会主义理论体系的重要组成部分，是全党全国人民为实现中华民族伟大复兴而奋斗的行动指南，必须长期坚持并不断发展。 <zijing.wu@qyer.com>
2019-02-18 10:53:33 +0800

    maze

---
+++ src/main/java/com/hhrb/ama/Nearest.java
@@ -0,0 +1,19 @@
+package com.hhrb.ama;
+
+/**
+ * User: Z J Wu Date: 2019-02-14 Time: 21:35 Package: com.hhrb.ama
+ */
+public class Nearest {
+  public static final class Coor{
+    private int x;
+    private int y;
+
+    public Coor(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+  public static void main(String[] args) {
+  }
+}
--- src/main/java/com/hhrb/leetcodecn/Wildcard.java
+++ src/main/java/com/hhrb/leetcodecn/Wildcard.java
@@ -22,9 +22,21 @@ public class Wildcard {
   public static final char ANYTHING_N = '*';
   public static final char ANYTHING_1 = '?';
 
+  public static int findLastMatch(String s, int fromIdx, char c) {
+    if (s == null || s.length() == 0) {
+      return -1;
+    }
+    for (int i = s.length() - 1; i >= fromIdx; i--) {
+      if (s.charAt(i) == c) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
   public static boolean match(String p, String s) {
-    System.out.println("P - " + p);
-    System.out.println("S - " + s);
+    System.out.println("模式: " + p);
+    System.out.println("需要检测的字符: " + s);
     System.out.println("---------------------------------");
     int sIdx = 0, pIdx = 0, pLen = p.length();
     // 记录*的位置
@@ -32,19 +44,21 @@ public class Wildcard {
     // 记录*匹配的s的位置
     int starMatchIdx = -1;
 
-    int counter=0;
+    int counter = 0;
     while (sIdx < s.length()) {
 
-      System.out.println("Round - "+counter);
-      System.out.println("PatternCharIdx - " + pIdx);
-      System.out.println("StringCharIdx - " + sIdx);
-      System.out.println("StarIdx - " + starIdx);
-      System.out.println("StarMatchStrIdx - " + starMatchIdx);
-      System.out.println("---------------------------------");
+      System.out.println("第" + counter + "轮");
+      String tmpS = pIdx >= pLen ? "pIdx超了p的长度" : String.valueOf(p.charAt(pIdx));
+      System.out.println("pIdx: " + pIdx + ", 对应的字是: " + tmpS);
+
+      tmpS = sIdx >= s.length() ? "sIdx超了s的长度" : String.valueOf(s.charAt(sIdx));
+      System.out.println("sIdx: " + sIdx + ", 对应的字是: " + tmpS);
+      System.out.println("starIdx: " + starIdx);
+      System.out.println("starMatchIdx: " + starMatchIdx);
 
       // 1. 最简单的情况, p和s完全相等, 或者p是?, 匹配任意1个s
       if (pIdx < pLen && (p.charAt(pIdx) == s.charAt(sIdx) || p.charAt(pIdx) == ANYTHING_1)) {
-        System.out.println(1);
+        System.out.println("进入条件 1");
         ++sIdx;
         ++pIdx;
       }
@@ -59,7 +73,7 @@ public class Wildcard {
        * *出现的位置, 以及当时s的位置
        * */
       else if (pIdx < pLen && p.charAt(pIdx) == ANYTHING_N) {
-        System.out.println(2);
+        System.out.println("进入条件 2");
         // 遇到*了, 单独记录一下*的位置
         starIdx = pIdx;
         // 同时, 记录一下这个*开始匹配的s的位置
@@ -70,19 +84,24 @@ public class Wildcard {
        *
        * */
       else if (starIdx != -1) {
-        System.out.println(3);
+        System.out.println("进入条件 3");
         pIdx = starIdx + 1;
-        ++starMatchIdx;
-        sIdx = starMatchIdx;
+        //        ++starMatchIdx;
+        //        sIdx = starMatchIdx;
+        ++sIdx;
       } else {
+        System.out.println("进入条件 4, 根本不匹配, 程序直接返回了, 不再进行后续操作");
         return false;
       }
       ++counter;
+      System.out.println("---------------------------------");
     }
     while (pIdx < pLen && p.charAt(pIdx) == ANYTHING_N) {
       ++pIdx;
     }
     // p恰巧消耗完, 不多不少, 说明匹配成功
+    System.out.println(pIdx);
+    System.out.println(pLen);
     return pIdx == pLen;
   }
 
@@ -93,8 +112,11 @@ public class Wildcard {
     //    testCases.add(new TestCase("cb", "*a"));
     //    testCases.add(new TestCase("adceb", "*a*b"));
     //    testCases.add(new TestCase("acdcb", "a*c?b"));
-    testCases.add(new TestCase("cdgiaacdfiad", "cd?i*d"));
+    //    testCases.add(new TestCase("cdgiaacdfiad", "cd?i*d"));
     //    testCases.add(new TestCase("abefcdgiescdfimde", "ab*cd?i*de"));
+    //    testCases.add(new TestCase("a12cf3cfz456b", "a*cfz*b"));
+    //    testCases.add(new TestCase("acc", "a*c"));
+    testCases.add(new TestCase("acb", "a*c"));
     testCases.stream().forEach(tc -> System.out.println(match(tc.pattern, tc.str)));
   }
 }
